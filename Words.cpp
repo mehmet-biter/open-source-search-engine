b4b993c06f5ee0eb3a027d30913580b35520eb00
Ai Lin Chia <alc@privacore.com>
2015-11-18 19:30:04 +0100

    Revert "Fix read of uninitializae data in Words::set()". This commit is causing coredump when 'string literal' is passed to Words::set. Reverting it until a better fix can be found.
    This reverts commit 413352e09e4f979f7511247b8a332001be45f0e3.

--- Words.cpp
+++ Words.cpp
@@ -47,9 +47,9 @@ bool Words::set ( char *s, int32_t slen, int32_t version,
 	}
 
 	char c = s[slen];
-	s[slen]='\0';
+	if ( c != '\0' ) s[slen]='\0';
 	bool status = set ( s , version, computeWordIds , niceness );
-	s[slen] = c;
+	if ( c != '\0' ) s[slen] = c;
 	return status;
 }
 
