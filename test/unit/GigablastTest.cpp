96a2604e8212da4b8d431bb975dfa54a275acf18
Ai Lin Chia <alc@privacore.com>
2015-11-19 14:43:52 +0100

    Move test to test from Words.cpp to WordsTest

--- test/unit/GigablastTest.cpp
+++ test/unit/GigablastTest.cpp
@@ -1,6 +1,7 @@
 #include "gtest/gtest.h"
 
 #include "Mem.h"
+#include "Unicode.h"
 
 int g_inMemcpy=0;
 bool g_recoveryMode = false;
@@ -15,11 +16,19 @@ bool sendPageSEO(TcpSocket *s, HttpRequest *hr) {
 }
 
 int main(int argc, char **argv) {
-	// initialize Gigablast variables
+	// initialize Gigablast
 	g_conf.m_maxMem = 1000000000LL;
+
 	g_mem.m_memtablesize = 8194*1024;
+	g_mem.init();
+
 	g_log.init("/dev/stdout");
 	g_conf.m_logDebugBuild = true;
+
+	if (!ucInit()) {
+		log("Unicode initialization failed!");
+		exit(1);
+	}
 
 	::testing::InitGoogleTest(&argc, argv);
 
---
+++ test/unit/WordsTest.cpp
@@ -0,0 +1,30 @@
+#include "gtest/gtest.h"
+
+#include "Words.h"
+
+TEST(WordsTest, VerifySize) {
+	// set c to a curling quote in unicode
+	int32_t c = 0x201c; // 0x235e;
+
+	// encode it into utf8
+	char dst[5];
+
+	// point to it
+	char *p = dst;
+
+	// put space in there
+	*p++ = ' ';
+
+	// "numBytes" is how many bytes it stored into 'dst"
+	int32_t numBytes = utf8Encode ( c , p );
+
+	// must be 2 bytes i guess
+	EXPECT_EQ(3, numBytes);
+
+	// check it
+	int32_t size = getUtf8CharSize(p);
+	EXPECT_EQ(3, size);
+
+	// is that punct
+	EXPECT_TRUE(is_punct_utf8(p));
+}
