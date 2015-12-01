e6061caf9d38e9b7bf0c302d133a67151eb58309
Ai Lin Chia <alc@privacore.com>
2015-12-01 12:54:58 +0100

    Minor modification of test

--- test/system/test_index_documents.py
+++ test/system/test_index_documents.py
@@ -1,22 +1,19 @@
 import pytest
-import os
+import mimetypes
 
 
-@pytest.mark.parametrize('file_location, url_with_file, content_type, expected_title', [
-    # file_location                     url_with_file   content_type            expected_title
-    ('test_word_no_properties.pdf',     True,           'application/pdf',      'test_word_no_properties.pdf'),
-    ('test_word_no_properties.pdf',     False,          'application/pdf',      ''),
-    ('test_word_with_properties.pdf',   True,           'application/pdf',      'Title for Microsoft Word (in title)'),
+@pytest.mark.parametrize('file_location, url_location, expected_title', [
+    # file_location                     url_location                        expected_title
+    ('test_word_no_properties.pdf',     'test_word_no_properties.pdf',      'test_word_no_properties.pdf'),
+    ('test_word_no_properties.pdf',     '',                                 ''),
+    ('test_word_with_properties.pdf',   'test_word_with_properties.pdf',    'Title for Microsoft Word (in title)'),
 ])
-def test_index_documents_office(gb_api, httpserver, file_location, url_with_file, content_type, expected_title):
+def test_index_documents_office(gb_api, httpserver, file_location, url_location, expected_title):
     httpserver.serve_content(content=open('data/office/' + file_location, 'rb').read(),
-                             headers={'content-type': content_type})
-    print(httpserver.url)
+                             headers={'content-type': mimetypes.guess_type(file_location)[0]})
 
     # format url
-    file_url = httpserver.url + '/'
-    if url_with_file:
-        file_url += os.path.basename(file_location)
+    file_url = httpserver.url + '/' + url_location
 
     # add url
     assert gb_api.add_url(file_url) == True
