ac35858f6608d89dfa4396a0ab33f29cc2422140
Ai Lin Chia <alc@privacore.com>
2015-12-17 16:05:43 +0100

    Don't allow gb to start when fatal_error file exist

--- gbcheck.sh
+++ gbcheck.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+working_dir=$(dirname $0)
+
+# Don't allow startup if fatal_error file exists
+if [ -f ${working_dir}/fatal_error ]; then
+    exit 1
+fi
+
 # verify system converters exist
 
 # pdf
