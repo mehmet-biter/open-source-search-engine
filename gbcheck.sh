feb701de80c05ce0bf669922aacd1be33ac1e6c9
Brian Rasmusson <br@privacore.com>
2015-12-18 23:32:49 +0100

    Store a list of current files before letting gb run. May ease debugging of file problems

--- gbcheck.sh
+++ gbcheck.sh
@@ -7,6 +7,10 @@ if [ -f ${working_dir}/fatal_error ]; then
     exit 1
 fi
 
+# Dump list of files before allowing gb to continue running
+find . -type f -exec ls -l --full-time {} \; 2> /dev/null |column -t|sort -k 9 > file_state_before_run.txt
+
+
 # verify system converters exist
 
 # pdf
