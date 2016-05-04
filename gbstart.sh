ab66aeeb6a6c91b52ea2323db6fd1af11e372b4c
Ai Lin Chia <alc@privacore.com>
2016-05-04 17:31:47 +0200

    Modify gbstart to read cpu affinity from taskset.conf file

--- gbstart.sh
+++ gbstart.sh
@@ -2,6 +2,24 @@
 
 working_dir=$(dirname $0)
 
+host_id=
+if [ $# -ge 1 ]; then
+	host_id=$1
+fi
+
+function get_cpu_affinity() {
+	if [ -z ${host_id} ]; then
+		return
+	fi
+
+	# do it the simple way until we decide on a strategy
+	if [ ! -f taskset.conf ]; then
+		return
+	fi
+
+	sed -n $(expr ${host_id} % 14 + 1)p taskset.conf
+}
+
 # we should use working directory
 cd ${working_dir}
 
@@ -10,12 +28,14 @@ if [ -f fatal_error ]; then
     exit 1
 fi
 
+
 ##################################
 # verify system converters exist #
 ##################################
 
 # pdf
 which pdftohtml >/dev/null 2>&1 || exit $?
+
 
 ############
 # Start gb #
@@ -38,20 +58,22 @@ while true; do
 	# in case gb was updated
 	mv -f gb.installed gb
 
-	./gb -l $ADDARGS
+	# leftover from previous run
+	rm -f cleanexit
+
+	GB_PRE=
+	cpu_affinity=$(get_cpu_affinity)
+	if [ ! -z ${cpu_affinity} ]; then
+		GB_PRE="taskset -c ${cpu_affinity}"
+	fi
+
+	${GB_PRE} ./gb -l $ADDARGS
 
 	EXITSTATUS=$?
 
 	# if gb does exit(0) then stop
-	if [ $EXITSTATUS = 0 ]; then
-		break
-	fi
-
 	# also stop if ./cleanexit is there because exit(0) does not always work for some strange reasons
-	if [ -f "cleanexit" ]; then
-		# make sure we don't affect next run
-		rm -f cleanexit
-
+	if [ $EXITSTATUS = 0 || -f "cleanexit" ]; then
 		break
 	fi
 
