02d39254f0b702cd012216d925fb844663344dd5
Ivan Skytte Jørgensen <isj-github@privacore.com>
2016-05-10 12:17:24 +0200

    Moved XmlDoc::getProbSpam() from header to .cpp

--- XmlDoc.cpp
+++ XmlDoc.cpp
@@ -22332,6 +22332,76 @@ bool XmlDoc::setSpam ( const int32_t *profile, int32_t plen , int32_t numWords ,
 	 return hadSpam;
 }
 
+
+// . returns 0 to 100 , the probability of spam for this subprofile
+// . a "profile" is an array of all the positions of a word in the document
+// . a "position" is just the word #, like first word, word #8, etc...
+// . we are passed a subprofile, "profile", of the actual profile
+//   because some of the document may be more "spammy" than other parts
+// . inlined to speed things up because this may be called multiple times
+//   for each word in the document
+// . if "step" is 1 we look at every       word position in the profile
+// . if "step" is 2 we look at every other word position
+// . if "step" is 3 we look at every 3rd   word position, etc...
+int32_t XmlDoc::getProbSpam(const int32_t *profile, int32_t plen, int32_t step) {
+
+	// you can spam 2 or 1 letter words all you want to
+	if ( plen <= 2 ) return 0;
+
+	// if our step is bigger than the profile return 0
+	if ( step == plen ) return 0;
+
+	register int32_t avgSpacing, stdDevSpacing;
+	int32_t d,dev=0;
+	register int32_t i;
+	
+	for (int32_t j = 0; j < step; j++) {
+
+		// find avg. of gaps between consecutive tokens in subprofile
+		// TODO: isn't profile[i] < profile[i+1]??
+		int32_t istop = plen-1;
+		avgSpacing = 0;
+		for (i=0; i < istop; i += step )
+			avgSpacing += ( profile[i] - profile[i+1] );
+		// there's 1 less spacing than positions in the profile
+		// so we divide by plen-1
+		avgSpacing = (avgSpacing * 256) / istop;
+
+		// compute standard deviation of the gaps in this sequence
+		stdDevSpacing = 0;
+		for (i = 0 ; i < istop; i += step ) {
+			d = (( profile[i] - profile[i+1]) * 256 ) - avgSpacing;
+			if ( d < 0 ) stdDevSpacing -= d;
+			else         stdDevSpacing += d;
+		}
+
+		// TODO: should we divide by istop-1 for stdDev??
+		stdDevSpacing /= istop;
+
+		// average of the stddevs for all sequences
+		dev += stdDevSpacing;
+	}
+
+	dev /= step;
+	
+	// if the plen is big we should expect dev to be big
+	// here's some interpolation points:
+	// plen >=  2  and  dev<= 0.2  --> 100%
+	// plen  =  7  and  dev = 1.0  --> 100%
+	// plen  = 14  and  dev = 2.0  --> 100%
+	// plen  = 21  and  dev = 3.0  --> 100%
+	// plen  = 7   and  dev = 2.0  -->  50%
+
+	// NOTE: dev has been multiplied by 256 to avoid using floats
+	if ( dev <= 51.2 ) return 100;  // (.2 * 256)
+	int32_t prob = ( (256*100/7) * plen ) / dev;
+
+	if (prob>100) prob=100;
+
+	return prob;
+}
+
+
 bool getWordPosVec ( Words *words ,
 		     Sections *sections,
 		     //int32_t wordStart,
