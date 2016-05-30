3c7440f769007ef69a1b4cd49861f23fd3bc3fdb
Ai Lin Chia <alc@privacore.com>
2016-05-30 17:12:00 +0200

    Fix conversion from string literal to 'char *' for Parms

--- Parms.cpp
+++ Parms.cpp
@@ -1142,7 +1142,7 @@ static DropLangs g_drops[] = {
 // in Collectiondb.cpp's CollectionRec::setUrlFiltersToDefaults().
 // for instance, UFP_NEWS spiders sites more frequently but less deep in
 // order to get "news" pages and articles
-bool printDropDownProfile ( SafeBuf* sb, char *name, CollectionRec *cr ) {
+bool printDropDownProfile ( SafeBuf* sb, const char *name, CollectionRec *cr ) {
 	sb->safePrintf ( "<select name=%s>", name );
 	// the type of url filters profiles
 	//char *items[] = {"custom","web","news","chinese","shallow"};
@@ -1163,7 +1163,7 @@ bool printDropDownProfile ( SafeBuf* sb, char *name, CollectionRec *cr ) {
 	return true;
 }
 
-bool printCheckBoxes ( int32_t n , SafeBuf* sb, char *name, char *array){
+bool printCheckBoxes ( int32_t n , SafeBuf* sb, const char *name, const char *array){
 	for ( int32_t i = 0 ; i < n ; i++ ) {
 		if ( i > 0 )
 			sb->safePrintf ("<input type=checkbox value=1 name=%s%" PRId32,
