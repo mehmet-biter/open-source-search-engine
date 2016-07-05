d4094f7dcdddac54681f7eb6b878253e75449ea1
Ivan Skytte Jørgensen <isj-github@privacore.com>
2016-07-05 12:18:50 +0200

    help flexelint

--- Sanity.h
+++ Sanity.h
@@ -2,6 +2,10 @@
 #define GB_SANITY_H
 
 // Ugly - but so is lots of code in .h files
+#ifndef __lint /*flexelint cannot handle c++11 yet*/
 [[ noreturn ]] void gbshutdownAbort( bool save_on_abort );
+#else
+void gbshutdownAbort( bool save_on_abort );
+#endif
 
 #endif // GB_SANITY_H
