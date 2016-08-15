mehmet-biter/open-source-search-engine

commit 3b80f31221ca293c93075a5d8736392c64f9efa0

Cleanup Msg51/Msg0
Msg51 stored stuff directly in Msg0. Changed to be kept in Msg51 per Msg0

Msg51.cpp
@@ -135,9 +135,11 @@ bool Msg51::getClusterRecs ( int64_t     *docIds                   ,
 	// reset these
 	m_numRequests = 0;
 	m_numReplies  = 0;
-	// clear these
-	for ( int32_t i = 0 ; i < MSG51_MAX_REQUESTS ; i++ )
-		m_msg0[i].m_inUse = false;
+	// clear/initialize these
+	for ( int32_t i = 0 ; i < MSG51_MAX_REQUESTS ; i++ ) {
+		m_slot[i].m_msg51 = this;
+		m_slot[i].m_inUse = false;
+	}
 	// . do gathering
 	// . returns false if blocked, true otherwise
 	// . send up to MSG51_MAX_REQUESTS requests at the same time
@@ -224,12 +226,14 @@ bool Msg51::sendRequests ( int32_t k ) {
 	if ( k >= MSG51_MAX_REQUESTS ) k = -1;
 
 	// if hint was provided use that
-	if ( k >= 0 && ! m_msg0[k].m_inUse ) slot = k;
+	if ( k >= 0 && ! m_slot[k].m_inUse )
+		slot = k;
 	// otherwise, do a scan for the empty slot
 	else {
 		for ( slot = 0 ; slot < MSG51_MAX_REQUESTS ; slot++ )
 			// break out if available
-			if ( ! m_msg0[slot].m_inUse ) break;
+			if(!m_slot[slot].m_inUse)
+				break;
 	}
 
 	// sanity check -- must have one!!
@@ -261,11 +265,8 @@ bool Msg51::sendRequest ( int32_t    i ) {
 	// advance so we do not do this docid again 
 	m_nexti++;
 
-	// use a hack to store this
-	m_msg0[i].m_parent  = this;
-	m_msg0[i].m_slot51  = i;
-	m_msg0[i].m_dataPtr = dataPtr;
-	m_msg0[i].m_inUse   = true;
+	m_slot[i].m_ci = m_nexti;
+	m_slot[i].m_inUse = true;
 	// count it
 	m_numRequests++;
 	// lookup in clusterdb, need a start and endkey
@@ -290,18 +291,18 @@ bool Msg51::sendRequest ( int32_t    i ) {
 	// . send the request for the cluster rec, use Msg0
 	// . returns false and sets g_errno on error
 	// . otherwise, it blocks and returns true
-	bool s = m_msg0[i].getList ( -1            , // hostid
+	bool s = m_slot[i].m_msg0.getList( -1            , // hostid
 				     -1            , // ip
 				     -1            , // port 
 				     m_maxCacheAge ,
 				     m_addToCache  ,
 				     RDB_CLUSTERDB ,
 				     m_collnum        ,
-				     &m_lists[i]   ,
+				     &m_slot[i].m_list,
 				     (char *)&startKey      ,
 				     (char *)&endKey        ,
 				     36            , // minRecSizes 
-				     &m_msg0[i]    , // state
+				     &m_slot[i],     // state
 				     gotClusterRecWrapper51  ,
 				     m_niceness    ,
 				     true        , // doErrorCorrection
@@ -313,7 +314,7 @@ bool Msg51::sendRequest ( int32_t    i ) {
 				     30000       , // timeout
 				     -1          , // syncPoint
 				     false       , // preferLocalReads
-				     &m_msg5[i]  , // use for local reads
+				     &m_slot[i].m_msg5, // use for local reads
 				     false       , // isRealMerge?
 				     true        , // allow page cache?
 				     false       , // force local indexdb?
@@ -329,21 +330,17 @@ bool Msg51::sendRequest ( int32_t    i ) {
 		return false; 
 	}
 	// otherwise, process the response
-	gotClusterRec ( &m_msg0[i] );
+	gotClusterRec ( &m_slot[i] );
 	return true;
 }
 
 void Msg51::gotClusterRecWrapper51(void *state) {
-	Msg0 *msg0 = (Msg0 *)state;
-	// extract our class form him -- a hack
-	Msg51 *THIS = (Msg51 *)msg0->m_parent;
-	// sanity check
-	if ( &THIS->m_msg0[msg0->m_slot51] != msg0 )
-		gbshutdownLogicError();
+	Slot *slot = static_cast<Slot*>(state);
+	Msg51 *THIS = slot->m_msg51;
 	// process it
-	THIS->gotClusterRec ( msg0 ) ;
+	THIS->gotClusterRec(slot);
 	// get slot number for re-send on this slot
-	int32_t    k = msg0->m_slot51;
+	int32_t    k = (int32_t)(slot-THIS->m_slot);
 	// . if not all done, launch the next one
 	// . this returns false if blocks, true otherwise
 	if ( ! THIS->sendRequests ( k ) ) return;
@@ -353,15 +350,15 @@ void Msg51::gotClusterRecWrapper51(void *state) {
 }
 
 // . sets m_errno to g_errno if not already set
-void Msg51::gotClusterRec ( Msg0 *msg0 ) { //, RdbList *list ) {
+void Msg51::gotClusterRec(Slot *slot) {
 
 	// count it
 	m_numReplies++;
 
 	// free up
-	msg0->m_inUse = false;
+	slot->m_inUse = false;
 
-	RdbList *list = msg0->m_list;
+	RdbList *list = slot->m_msg0.m_list;
 
 	// update m_errno if we had an error
 	if ( ! m_errno ) m_errno = g_errno;
@@ -377,7 +374,7 @@ void Msg51::gotClusterRec ( Msg0 *msg0 ) { //, RdbList *list ) {
 	//int64_t docId = g_clusterdb.getDocId ( *startKey );
 
 	// this doubles as a ptr to a cluster rec
-	int32_t    ci = (int32_t   )(PTRTYPE)msg0->m_dataPtr;
+	int32_t    ci = slot->m_ci;
 	// get docid
 	int64_t docId = m_docIds[ci];
 	// assume error!

Msg0.h
@@ -134,12 +134,6 @@ class Msg0 {
 
 	// for allowing the page cache
 	bool m_allowPageCache;
-
-	// this is a hack so Msg51 can store his this ptr here
-	void *m_parent;  // used by Msg51 and by Msg2.cpp
-	int32_t  m_slot51;  // for resending on same Msg0 slot in array
-	void *m_dataPtr; // for holding recepient record ptr of TopNode ptr
-	char  m_inUse;
 };
 
 #endif // GB_MSG0_H

Msg51.h
@@ -135,8 +135,6 @@ class Msg51 {
 	bool sendRequests   ( int32_t k );
 	bool sendRequest    ( int32_t i );
 
-	void gotClusterRec  ( class Msg0 *msg0 );
-
 	// docIds we're getting clusterRecs for
 	int64_t   *m_docIds;
 	int32_t         m_numDocIds;
@@ -174,12 +172,19 @@ class Msg51 {
 	// for super quick disk page cache lookups
 	//Msg5       m_msg5;
 
-	Msg0       m_msg0  [ MSG51_MAX_REQUESTS ];
-	RdbList    m_lists [ MSG51_MAX_REQUESTS ];
-	Msg5       m_msg5  [ MSG51_MAX_REQUESTS ];
-
 private:
+	struct Slot {
+		Msg51     *m_msg51; //points to self
+		Msg0       m_msg0;
+		RdbList    m_list;
+		Msg5       m_msg5;
+		bool       m_inUse;
+		int32_t    m_ci;
+	};
+	Slot m_slot[MSG51_MAX_REQUESTS];
+
 	static void gotClusterRecWrapper51(void *state);
+	void gotClusterRec(Slot *slot);
 };
 
 

commit 2c596915828d7e3aedd5d9da53fede9b0e7ae6da

Bugfix off-by-one in clustering (Msg51)
Bug introduced in commit 3b80f31221ca293c93075a5d8736392c64f9efa0

Msg51.cpp
@@ -265,7 +265,7 @@ bool Msg51::sendRequest ( int32_t    i ) {
 	// advance so we do not do this docid again 
 	m_nexti++;
 
-	m_slot[i].m_ci = m_nexti;
+	m_slot[i].m_ci = ci;
 	m_slot[i].m_inUse = true;
 	// count it
 	m_numRequests++;