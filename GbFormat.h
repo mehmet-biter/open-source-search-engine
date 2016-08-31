64bfbc65d4cb6e69cd153263effd71bd01a5dc12
Ivan Skytte Jørgensen <isj-github@privacore.com>
2016-08-31 19:14:16 +0200

    Moved format #define out from HttpRequest.h so more code doesn't need to know the innaers of HttpRequest

---
+++ GbFormat.h
@@ -0,0 +1,15 @@
+#ifndef GBFORMAT_H_
+#define GBFORMAT_H_
+
+// values for HttpRequest::m_replyFormat
+#define FORMAT_HTML 1
+#define FORMAT_XML  2
+#define FORMAT_JSON 3
+#define FORMAT_CSV  4
+#define FORMAT_TXT  5
+#define FORMAT_WIDGET_IFRAME 7
+#define FORMAT_WIDGET_AJAX 8
+// used by ajax widget to create search results to APPEND to the end of widget
+#define FORMAT_WIDGET_APPEND 9
+
+#endif //GBFORMAT_H_
--- HttpRequest.h
+++ HttpRequest.h
@@ -27,16 +27,7 @@
 #include "Url.h"       // Url class
 #include "TcpSocket.h"
 
-// values for HttpRequest::m_replyFormat
-#define FORMAT_HTML 1
-#define FORMAT_XML  2
-#define FORMAT_JSON 3
-#define FORMAT_CSV  4
-#define FORMAT_TXT  5
-#define FORMAT_WIDGET_IFRAME 7
-#define FORMAT_WIDGET_AJAX 8
-// used by ajax widget to create search results to APPEND to the end of widget
-#define FORMAT_WIDGET_APPEND 9
+#include "GbFormat.h"
 
 class HttpRequest {
 
