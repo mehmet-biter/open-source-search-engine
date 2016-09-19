13cfcb6717ea0eeb72434c8426a1ca5c22c2dbfc
Ai Lin Chia <alc@privacore.com>
2016-09-19 12:13:24 +0200

    Add unit test for Posdb

--- test/unit/GigablastTest.cpp
+++ test/unit/GigablastTest.cpp
@@ -4,12 +4,30 @@
 #include "Unicode.h"
 #include "hash.h"
 #include "Conf.h"
+#include "Hostdb.h"
+
+#include <stdlib.h>
+#include <libgen.h>
+#include <limits.h>
 
 int main(int argc, char **argv) {
-	// initialize Gigablast
-	g_conf.m_maxMem = 1000000000LL;
-
+	// initialize library
 	g_mem.init();
+	hashinit();
+
+	// current dir
+	char tmpPath[PATH_MAX];
+	realpath(argv[0], tmpPath);
+
+	char currentPath[PATH_MAX];
+	strcpy(currentPath, dirname(tmpPath));
+	size_t currentPathLen = strlen(currentPath);
+	if (currentPath[currentPathLen] != '/') {
+		strcat(currentPath, "/");
+	}
+
+	g_hostdb.init(-1, NULL, false, false, currentPath);
+	g_conf.init(NULL);
 
 	g_log.init("/dev/stdout");
 
@@ -17,8 +35,6 @@ int main(int argc, char **argv) {
 		log("Unicode initialization failed!");
 		exit(1);
 	}
-
-	hashinit();
 
 	::testing::InitGoogleTest(&argc, argv);
 
