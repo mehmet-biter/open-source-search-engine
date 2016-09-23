2b86d73a4ce8da8c5f5357e213bfd0a819041383
Brian Rasmusson <br@privacore.com>
2016-09-23 13:49:25 +0200

    fix dead code in Process::saveRdbTrees

--- Process.cpp
+++ Process.cpp
@@ -1002,14 +1002,15 @@ bool Process::saveRdbTrees ( bool useThread , bool shuttingDown ) {
 	// only save spiderdb based trees if shutting down so we can
 	// still write to them without writes being disabled
 	if ( ! shuttingDown ) {
+		// . check spider cache files (doleiptable waitingtree etc.)
+		// . this should return true if it still has some files that haven't
+		//   saved to disk yet... so if it returns true we return false 
+		//   indicating that we are still waiting!
+		if ( g_spiderCache.needsSave () ) {
+			return false;
+		}
 		return true;
 	}
-
-	// . check spider cache files (doleiptable waitingtree etc.)
-	// . this should return true if it still has some files that haven't
-	//   saved to disk yet... so if it returns true we return false 
-	//   indicating that we are still waiting!
-	if ( ! shuttingDown && g_spiderCache.needsSave () ) return false;
 
 	// reset for next call
 	m_calledSave = false;
