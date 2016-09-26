65949781396e7c5194c438cc93a15e9a1df49471
Brian Rasmusson <br@privacore.com>
2016-09-26 14:18:26 +0200

    init class members in State22 (Msg22)

--- Msg22.cpp
+++ Msg22.cpp
@@ -270,7 +270,19 @@ public:
 	// free slot request here too
 	char *m_slotReadBuf;
 	int32_t  m_slotAllocSize;
-	State22() {m_slotReadBuf = NULL;}
+
+	State22() {
+		m_slot = NULL;
+		m_pd = 0;
+		m_docId1 = 0;
+		m_docId2 = 0;
+		m_availDocId = 0;
+		m_uh48 = 0;
+		m_r = NULL;
+		m_slotReadBuf = NULL;
+		m_slotAllocSize = 0;
+	}
+
 	~State22() {
 		if ( m_slotReadBuf )
 			mfree(m_slotReadBuf,m_slotAllocSize,"st22");
