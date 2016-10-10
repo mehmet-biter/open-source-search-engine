7875f44f94794dffa48e1b65ad1277fa0b296958
Ai Lin Chia <alc@privacore.com>
2016-10-10 14:37:14 +0200

    Remove debug logs

--- test/unit/RdbBaseTest.cpp
+++ test/unit/RdbBaseTest.cpp
@@ -46,28 +46,22 @@ TEST_P(RdbBasePosdbIndexSingleDocTest, PosdbGenerateIndexSingleDocId) {
 	int64_t termId0 = ::testing::get<0>(GetParam());
 	GbTest::addPosdbKey(index0, termId0, docId, 0, ::testing::get<0>(GetParam()) == POSDB_DELETEDOC_TERMID);
 	index0->writeIndex();
-	index0->printIndex();
-
 
 	ASSERT_EQ(1, base->addNewFile(-1));
 	RdbIndex *index1 = base->getIndex(1);
 	int64_t termId1 = ::testing::get<1>(GetParam());
 	GbTest::addPosdbKey(index1, termId1, docId, 0, ::testing::get<1>(GetParam()) == POSDB_DELETEDOC_TERMID);
 	index1->writeIndex();
-	index1->printIndex();
 
 	ASSERT_EQ(2, base->addNewFile(-1));
 	RdbIndex *index2 = base->getIndex(2);
 	int64_t termId2 = ::testing::get<2>(GetParam());
 	GbTest::addPosdbKey(index2, termId2, docId, 0, ::testing::get<2>(GetParam()) == POSDB_DELETEDOC_TERMID);
 	index2->writeIndex();
-	index2->printIndex();
 
 	base->generateGlobalIndex();
 	auto globalIndex = base->getGlobalIndex();
 	ASSERT_EQ(1, globalIndex->size());
 	int64_t result = (((docId << RdbIndex::s_docIdOffset) | (!termId2 == POSDB_DELETEDOC_TERMID)) << RdbBase::s_docIdFileIndex_docIdOffset | 2);
-	logf(LOG_TRACE, "inputDocId=%lld result=%llx first=%llx", docId, result, *globalIndex->begin());
 	EXPECT_EQ(result, *globalIndex->begin());
-	base->printGlobalIndex();
 }
