f7e34ec55cfead200b5b9bb0ab4e4b3c1abe73ab
Ai Lin Chia <alc@privacore.com>
2016-11-07 12:16:54 +0100

    Get build-wrapper for running sonarqube analysis

---
+++ runSonarQubeAnalysis.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# Exit on failure
+set -e
+
+installBuildWrapper() {
+	export SONAR_HOME=$HOME/.sonar
+	mkdir -p $SONAR_HOME
+	curl -sSLo $SONAR_HOME/build-wrapper-linux-x86.zip https://sonarqube.com/static/cpp/build-wrapper-linux-x86.zip
+	unzip $SONAR_HOME/build-wrapper-linux-x86.zip -d $SONAR_HOME
+	rm $SONAR_HOME/build-wrapper-linux-x86.zip
+	export PATH=$SONAR_HOME/build-wrapper-linux-x86:$PATH
+}
+
+# Install the build-wrapper
+installBuildWrapper
+
+# make using build-wrapper
+build-wrapper-linux-x86-64 make
+
+# And run the analysis
+sonar-scanner -Dsonar.login=$SONAR_TOKEN
+
