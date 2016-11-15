7a09a20197ea0925dc9ca717d008e4fb4efce3b1
Ai Lin Chia <alc@privacore.com>
2016-11-15 14:52:43 +0100

    Make Spiderdb function static

--- Spider.h
+++ Spider.h
@@ -423,33 +423,33 @@ class Spiderdb {
 	// this rdb holds urls waiting to be spidered or being spidered
 	Rdb m_rdb;
 
-	int64_t getUrlHash48( key128_t *k ) {
+	static int64_t getUrlHash48( key128_t *k ) {
 		return (((k->n1)<<16) | k->n0>>(64-16)) & 0xffffffffffffLL;
 	}
 	
-	bool isSpiderRequest( key128_t *k ) {
+	static bool isSpiderRequest( key128_t *k ) {
 		return (k->n0>>(64-17))&0x01;
 	}
 
-	bool isSpiderReply( key128_t *k ) {
+	static bool isSpiderReply( key128_t *k ) {
 		return ((k->n0>>(64-17))&0x01)==0x00;
 	}
 
-	int64_t getParentDocId( key128_t *k ) {
+	static int64_t getParentDocId( key128_t *k ) {
 		return (k->n0>>9)&DOCID_MASK;
 	}
 
-	int32_t getFirstIp( key128_t *k ) {
+	static int32_t getFirstIp( key128_t *k ) {
 		return (k->n1>>32);
 	}
-	
-	key128_t makeKey( int32_t firstIp, int64_t urlHash48, bool isRequest, int64_t parentDocId, bool isDel );
 
-	key128_t makeFirstKey( int32_t firstIp ) {
+	static key128_t makeKey( int32_t firstIp, int64_t urlHash48, bool isRequest, int64_t parentDocId, bool isDel );
+
+	static key128_t makeFirstKey( int32_t firstIp ) {
 		return makeKey( firstIp, 0LL, false, 0LL, true );
 	}
 
-	key128_t makeLastKey( int32_t firstIp ) {
+	static key128_t makeLastKey( int32_t firstIp ) {
 		return makeKey( firstIp, 0xffffffffffffLL, true, MAX_DOCID, false );
 	}
 
