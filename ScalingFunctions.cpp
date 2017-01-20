9512f7991d7c7cdd5fb7096e43afa3dd0ba11bf4
Ivan Skytte Jørgensen <isj-github@privacore.com>
2017-01-20 15:03:40 +0100

    Prevent division by zero in scaling functions

--- ScalingFunctions.cpp
+++ ScalingFunctions.cpp
@@ -5,6 +5,7 @@ double scale_linear(double x, double min_x, double max_x, double min_y, double m
 	if(x<min_x) x=min_x;
 	if(x>max_x) x=max_x;
 	double x_range = max_x-min_x;
+	if(x_range==0.0) return min_y; //don't divide by zero
 	double y_range = max_y-min_y;
 	double r = (x-min_x)/x_range;
 	double y = min_y + r*y_range;
@@ -16,6 +17,7 @@ float scale_linear(float x, float min_x, float max_x, float min_y, float max_y)
 	if(x<min_x) x=min_x;
 	if(x>max_x) x=max_x;
 	float x_range = max_x-min_x;
+	if(x_range==0.0) return min_y; //don't divide by zero
 	float y_range = max_y-min_y;
 	float r = (x-min_x)/x_range;
 	float y = min_y + r*y_range;
@@ -30,6 +32,7 @@ double scale_quadratic(double x, double min_x, double max_x, double min_y, doubl
 	if(x<min_x) x=min_x;
 	if(x>max_x) x=max_x;
 	double x_range = max_x-min_x;
+	if(x_range==0.0) return min_y; //don't divide by zero
 	double y_range = max_y-min_y;
 	double r = (x-min_x)/x_range;
 	return ((r+1)*(r+1)-1)/3*y_range+min_y;
