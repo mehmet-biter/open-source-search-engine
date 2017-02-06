mehmet-biter/open-source-search-engine

commit 54349265d448c0d8be4396f4872bf4bb24b6c6a1

Moved SafeBuf::cdataEncode() to a standalone function

GbUtil.cpp
@@ -1,0 +1,37 @@
+#include "GbUtil.h"
+#include "SafeBuf.h"
+#include <string.h>
+
+bool cdataEncode(SafeBuf *dstBuf, const char *src) {
+	return cdataEncode(dstBuf,src,strlen(src));
+}
+
+
+bool cdataEncode(SafeBuf *dstBuf, const char *src, size_t len) {
+	if(!dstBuf->reserve(len))
+		return false;
+	const char *endptr = src+len;
+	const char *early_endptr = endptr-2;
+	for(const char *s = src; s<early_endptr; ) {
+		bool b;
+		if(s[0]!=']' || s[1]!=']' || s[2]!='>') {
+			b = dstBuf->pushChar(*s);
+			s += 1;
+		} else {
+			//turn ]]> into ]]&gt;
+			b = dstBuf->pushChar(']') &&
+			    dstBuf->pushChar(']') &&
+			    dstBuf->pushChar('&') &&
+			    dstBuf->pushChar('g') &&
+			    dstBuf->pushChar('t') &&
+			    dstBuf->pushChar(';');
+			s += 3;
+		}
+		if(!b) return false;
+	}
+	//handle 2-byte tail
+	for(const char *s=early_endptr; s<endptr; s++)
+		if(!dstBuf->pushChar(*s))
+			return false;
+	return true;
+}

GbUtil.h
@@ -1,0 +1,14 @@
+#ifndef GB_UTIL_H_
+#define GB_UTIL_H_
+
+//Kitchen sink
+
+#include <stddef.h>
+
+class SafeBuf;
+
+//Append string into safebuf, replaing all occurences of ]]> with ]]&gt;
+bool cdataEncode(SafeBuf *dstBuf, const char *src);
+bool cdataEncode(SafeBuf *dstBuf, const char *src, size_t len);
+
+#endif

HttpServer.cpp
@@ -13,6 +13,7 @@
 #include "PageRoot.h"
 #include "GigablastRequest.h"
 #include "Process.h"
+#include "GbUtil.h"
 
 #ifdef _VALGRIND_
 #include <valgrind/memcheck.h>
@@ -1436,7 +1437,7 @@ bool HttpServer::sendErrorReply ( GigablastRequest *gr ) {
 		xb.safePrintf("<response>\n"
 			      "\t<statusCode>%" PRId32"</statusCode>\n"
 			      "\t<statusMsg><![CDATA[", error );
-		xb.cdataEncode(errmsg );
+		cdataEncode(&xb, errmsg);
 		xb.safePrintf("]]></statusMsg>\n"
 			      "</response>\n");
 	}
@@ -1530,7 +1531,7 @@ bool HttpServer::sendErrorReply ( TcpSocket *s , int32_t error , const char *err
 		xb.safePrintf("<response>\n"
 			      "\t<statusCode>%" PRId32"</statusCode>\n"
 			      "\t<statusMsg><![CDATA[", error );
-		xb.cdataEncode(errmsg );
+		cdataEncode(&xb, errmsg);
 		xb.safePrintf("]]></statusMsg>\n"
 			      "</response>\n");
 	}

Makefile
@@ -65,6 +65,7 @@ OBJS_O3 = \
 	TopTree.o \
 	UrlComponent.o UrlParser.o UdpStatistic.o \
 	MergeSpaceCoordinator.o \
+	GbUtil.o \
 
 
 OBJS = $(OBJS_O0) $(OBJS_O1) $(OBJS_O2) $(OBJS_O3)

PageGet.cpp
@@ -11,6 +11,7 @@
 #include "Tagdb.h"
 #include "XmlDoc.h"
 #include "Process.h"
+#include "GbUtil.h"
 
 // TODO: redirect to host that has the titleRec locally
 
@@ -604,7 +605,7 @@ bool processLoop ( void *state ) {
 		sb->safePrintf("<statusCode>0</statusCode>\n");
 		sb->safePrintf("<statusMsg>Success</statusMsg>\n");
 		sb->safePrintf("<url><![CDATA[");
-		sb->cdataEncode(xd->m_firstUrl.getUrl());
+		cdataEncode(sb, xd->m_firstUrl.getUrl());
 		sb->safePrintf("]]></url>\n");
 		sb->safePrintf("<docId>%" PRIu64"</docId>\n",xd->m_docId);
 		sb->safePrintf("\t<cachedTimeUTC>%" PRId32"</cachedTimeUTC>\n",
@@ -806,7 +807,7 @@ bool processLoop ( void *state ) {
 
 	if ( format == FORMAT_XML ) {
 		sb->safePrintf("\t<content><![CDATA[");
-		sb->cdataEncode ( xb->getBufStart() );
+		cdataEncode(sb, xb->getBufStart());
 		sb->safePrintf("]]></content>\n");
 		sb->safePrintf("</response>\n");
 	}

PageHosts.cpp
@@ -6,6 +6,7 @@
 #include "Hostdb.h"
 #include "sort.h"
 #include "Conf.h"
+#include "GbUtil.h"
 
 static int defaultSort    ( const void *i1, const void *i2 );
 static int pingSort1      ( const void *i1, const void *i2 );
@@ -603,7 +604,7 @@ skipReplaceHost:
 			sb.safePrintf("\t<host>\n"
 				      "\t\t<name><![CDATA["
 				      );
-			sb.cdataEncode (h->m_hostname);
+			cdataEncode(&sb, h->m_hostname);
 			sb.safePrintf("]]></name>\n");
 			sb.safePrintf("\t\t<shard>%" PRId32"</shard>\n",
 				      (int32_t)h->m_shardNum);

PageInject.cpp
@@ -9,6 +9,7 @@
 #include "HttpRequest.h"
 #include "Stats.h"
 #include "Process.h"
+#include "GbUtil.h"
 
 
 static bool sendHttpReply        ( void *state );
@@ -368,7 +369,7 @@ bool sendHttpReply ( void *state ) {
 		am.safePrintf("\t<statusCode>%" PRId32"</statusCode>\n",
 			      (int32_t)g_errno);
 		am.safePrintf("\t<statusMsg><![CDATA[");
-		am.cdataEncode(mstrerror(g_errno));
+		cdataEncode(&am, mstrerror(g_errno));
 		am.safePrintf("]]></statusMsg>\n");
 
 		am.safePrintf("\t<docId>%" PRId64"</docId>\n",docId);

PageResults.cpp
@@ -28,6 +28,7 @@
 #include "Process.h"
 #include "Linkdb.h"
 #include "XmlDoc.h"
+#include "GbUtil.h"
 
 static bool printSearchFiltersBar ( SafeBuf *sb , HttpRequest *hr ) ;
 static bool printMenu ( SafeBuf *sb , int32_t menuNum , HttpRequest *hr ) ;
@@ -1027,7 +1028,7 @@ bool printSearchResultsHeader ( State0 *st ) {
 		Query *q = &si->m_q;
 		sb->safePrintf("\t<queryInfo>\n");
 		sb->safePrintf("\t\t<fullQuery><![CDATA[");
-		sb->cdataEncode(q->m_orig);
+		cdataEncode(sb, q->m_orig);
 		sb->safePrintf("]]></fullQuery>\n");
 		sb->safePrintf("\t\t<queryLanguageAbbr>"
 			       "<![CDATA[%s]]>"
@@ -1065,7 +1066,7 @@ bool printSearchResultsHeader ( State0 *st ) {
 			sb->safePrintf("\t\t\t<termStr><![CDATA[");
 			const char *printTerm = qt->m_term;
 			if ( is_wspace_a(term[0])) printTerm++;
-			sb->cdataEncode(printTerm);
+			cdataEncode(sb, printTerm);
 			sb->safePrintf("]]>"
 				       "</termStr>\n");
 			term[qt->m_termLen] = c;
@@ -2132,7 +2133,7 @@ bool printResult ( State0 *st, int32_t ix , int32_t *numPrintedSoFar ) {
 
 	if ( mr->ptr_content && si->m_format == FORMAT_XML ) {
 		sb->safePrintf("\t\t<content><![CDATA[" );
-		sb->cdataEncode ( mr->ptr_content );
+		cdataEncode(sb, mr->ptr_content);
 		sb->safePrintf("]]></content>\n");
 	}
 		
@@ -2296,7 +2297,7 @@ bool printResult ( State0 *st, int32_t ix , int32_t *numPrintedSoFar ) {
 				       "</origImageWidth>\n",
 				       ti->m_origDX);
 			sb->safePrintf("\t\t<imageUrl><![CDATA[");
-			sb->cdataEncode(ti->getUrl());
+			cdataEncode(sb, ti->getUrl());
 			sb->safePrintf("]]></imageUrl>\n");
 		}
 		if ( si->m_format == FORMAT_JSON ) {
@@ -2541,7 +2542,7 @@ bool printResult ( State0 *st, int32_t ix , int32_t *numPrintedSoFar ) {
 	if ( si->m_format == FORMAT_XML ) {
 		sb->safePrintf("\t\t<title><![CDATA[");
 		if ( str ) {
-			sb->cdataEncode(str);
+			cdataEncode(sb, str);
 		}
 		sb->safePrintf("]]></title>\n");
 	}
@@ -2573,7 +2574,7 @@ bool printResult ( State0 *st, int32_t ix , int32_t *numPrintedSoFar ) {
 	for ( ; hp && hp < hpend ; ) {
 		if ( si->m_format == FORMAT_XML ) {
 			sb->safePrintf("\t\t<h1Tag><![CDATA[");
-			sb->cdataEncode(hp);
+			cdataEncode(sb, hp);
 			sb->safePrintf("]]></h1Tag>\n");
 		}
 		if ( si->m_format == FORMAT_JSON ) {
@@ -2688,7 +2689,7 @@ bool printResult ( State0 *st, int32_t ix , int32_t *numPrintedSoFar ) {
 		sb->safePrintf( "<br>\n" );
 	} else if ( si->m_format == FORMAT_XML ) {
 		sb->safePrintf( "\t\t<sum><![CDATA[" );
-		sb->cdataEncode( str );
+		cdataEncode(sb, str);
 		sb->safePrintf( "]]></sum>\n" );
 	} else if ( si->m_format == FORMAT_JSON ) {
 		sb->safePrintf( "\t\t\"sum\":\"" );
@@ -5539,7 +5540,7 @@ static bool printMetaContent ( Msg40 *msg40 , int32_t i , State0 *st, SafeBuf *s
 			if ( si->m_format == FORMAT_XML ) {
 				sb->safePrintf ( "\t\t<display name=\"%s\">"
 					  	"<![CDATA[", ss );
-				sb->cdataEncode ( dptr, ddlen );
+				cdataEncode(sb, dptr, ddlen);
 				sb->safePrintf ( "]]></display>\n" );
 			}
 			else if ( si->m_format == FORMAT_JSON ) {

commit 589c450fcb49cadbda8e547c39ebb528ebbddc35

bugfix cdataEncode() to handle inputs shorter than 3 bytes
Bug introduced in 54349265d448c0d8be4396f4872bf4bb24b6c6a1. Effect including invalid XML when summaries where shorter than 3 bytes.

GbUtil.cpp
@@ -8,6 +8,8 @@ bool cdataEncode(SafeBuf *dstBuf, const char *src) {
 
 
 bool cdataEncode(SafeBuf *dstBuf, const char *src, size_t len) {
+	if(len<3)
+		return dstBuf->safeMemcpy(src,len);
 	if(!dstBuf->reserve(len))
 		return false;
 	const char *endptr = src+len;