94fad65e3c8fba72617216cda57c47056df2f6f5
Ai Lin Chia <alc@privacore.com>
2017-03-24 14:15:03 +0100

    Sync changes from nomerge2 for get_titlerec

--- tools/get_titlerec.cpp
+++ tools/get_titlerec.cpp
@@ -90,65 +90,71 @@ int main(int argc, char **argv) {
 	key96_t startKey = Titledb::makeFirstKey(docId);
 	key96_t endKey = Titledb::makeLastKey(docId);
 
-	msg5.getList(RDB_TITLEDB, 0, &list, startKey, endKey, 500000000, true, 0, 0, -1, NULL, NULL, 0, true, NULL, 0, -1, -1LL, false, true);
 
-	if (list.getNumRecs() != 1) {
+	if (!msg5.getList(RDB_TITLEDB, 0, &list, startKey, endKey, 500000000, true, 0, 0, -1, NULL, NULL, 0, true, NULL, 0, -1, -1LL, false, true)) {
+		logf(LOG_TRACE, "msg5.getlist didn't block");
+		cleanup();
+		exit(1);
+	}
+
+	if (list.isEmpty()) {
 		logf(LOG_TRACE, "Unable to find titlerec for docId=%" PRIu64, docId);
 		cleanup();
 		exit(1);
 	}
 
-	XmlDoc xmlDoc;
-	if (!xmlDoc.set2(list.getCurrentRec(), list.getCurrentRecSize(), "main", NULL, 0)) {
-		logf(LOG_TRACE, "Unable to set XmlDoc for docId=%" PRIu64, docId);
-		cleanup();
-		exit(1);
-	}
+	for (list.resetListPtr(); !list.isExhausted(); list.skipCurrentRecord()) {
+		XmlDoc xmlDoc;
+		if (!xmlDoc.set2(list.getCurrentRec(), list.getCurrentRecSize(), "main", NULL, 0)) {
+			logf(LOG_TRACE, "Unable to set XmlDoc for docId=%" PRIu64, docId);
+			cleanup();
+			exit(1);
+		}
 
-	logf(LOG_TRACE, "XmlDoc info");
-	logf(LOG_TRACE, "\tfirstUrl   : %.*s", xmlDoc.size_firstUrl, xmlDoc.ptr_firstUrl);
-	logf(LOG_TRACE, "\tredirUrl   : %.*s", xmlDoc.size_redirUrl, xmlDoc.ptr_redirUrl);
-	logf(LOG_TRACE, "\trootTitle  : %.*s", xmlDoc.size_rootTitleBuf, xmlDoc.ptr_rootTitleBuf);
-//	logf(LOG_TRACE, "\timageData  :");
-	logf(LOG_TRACE, "\t");
-	loghex(LOG_TRACE, xmlDoc.ptr_utf8Content, xmlDoc.size_utf8Content, "\tutf8Content:");
-	logf(LOG_TRACE, "\tsite       : %.*s", xmlDoc.size_site, xmlDoc.ptr_site);
+		logf(LOG_TRACE, "XmlDoc info");
+		logf(LOG_TRACE, "\tfirstUrl   : %.*s", xmlDoc.size_firstUrl, xmlDoc.ptr_firstUrl);
+		logf(LOG_TRACE, "\tredirUrl   : %.*s", xmlDoc.size_redirUrl, xmlDoc.ptr_redirUrl);
+		logf(LOG_TRACE, "\trootTitle  : %.*s", xmlDoc.size_rootTitleBuf, xmlDoc.ptr_rootTitleBuf);
+		//	logf(LOG_TRACE, "\timageData  :");
+		logf(LOG_TRACE, "\t");
+		loghex(LOG_TRACE, xmlDoc.ptr_utf8Content, xmlDoc.size_utf8Content, "\tutf8Content:");
+		logf(LOG_TRACE, "\tsite       : %.*s", xmlDoc.size_site, xmlDoc.ptr_site);
 
-	logf(LOG_TRACE, "\tlinkInfo");
-	LinkInfo* linkInfo = xmlDoc.getLinkInfo1();
-	logf(LOG_TRACE, "\t\tm_numGoodInlinks     : %d", linkInfo->m_numGoodInlinks);
-	logf(LOG_TRACE, "\t\tm_numInlinksInternal : %d", linkInfo->m_numInlinksInternal);
-	logf(LOG_TRACE, "\t\tm_numStoredInlinks   : %d", linkInfo->m_numStoredInlinks);
+		logf(LOG_TRACE, "\tlinkInfo");
+		LinkInfo *linkInfo = xmlDoc.getLinkInfo1();
+		logf(LOG_TRACE, "\t\tm_numGoodInlinks     : %d", linkInfo->m_numGoodInlinks);
+		logf(LOG_TRACE, "\t\tm_numInlinksInternal : %d", linkInfo->m_numInlinksInternal);
+		logf(LOG_TRACE, "\t\tm_numStoredInlinks   : %d", linkInfo->m_numStoredInlinks);
 
-	int i = 0;
-	for (Inlink *inlink = linkInfo->getNextInlink(NULL); inlink; inlink = linkInfo->getNextInlink(inlink)) {
-		logf(LOG_TRACE, "\t\tinlink #%d", i++);
-		logf(LOG_TRACE, "\t\t\tdocId        : %" PRIu64, inlink->m_docId);
-		logf(LOG_TRACE, "\t\t\turl          : %s", inlink->getUrl());
-		logf(LOG_TRACE, "\t\t\tlinktext     : %s", inlink->getLinkText());
-		logf(LOG_TRACE, "\t\t\tcountry      : %s", getCountryCode(inlink->m_country));
-		logf(LOG_TRACE, "\t\t\tlanguage     : %s", getLanguageAbbr(inlink->m_language));
-	}
+		int i = 0;
+		for (Inlink *inlink = linkInfo->getNextInlink(NULL); inlink; inlink = linkInfo->getNextInlink(inlink)) {
+			logf(LOG_TRACE, "\t\tinlink #%d", i++);
+			logf(LOG_TRACE, "\t\t\tdocId        : %" PRIu64, inlink->m_docId);
+			logf(LOG_TRACE, "\t\t\turl          : %s", inlink->getUrl());
+			logf(LOG_TRACE, "\t\t\tlinktext     : %s", inlink->getLinkText());
+			logf(LOG_TRACE, "\t\t\tcountry      : %s", getCountryCode(inlink->m_country));
+			logf(LOG_TRACE, "\t\t\tlanguage     : %s", getLanguageAbbr(inlink->m_language));
+		}
 
-	loghex(LOG_TRACE, xmlDoc.ptr_linkdbData, xmlDoc.size_linkdbData, "\tlinkdbData");
+		loghex(LOG_TRACE, xmlDoc.ptr_linkdbData, xmlDoc.size_linkdbData, "\tlinkdbData");
 
-	logf(LOG_TRACE, "\ttagRec");
-	TagRec *tagRec = xmlDoc.getTagRec();
-	for (Tag *tag = tagRec->getFirstTag(); tag; tag = tagRec->getNextTag(tag)) {
-		SafeBuf sb;
-		tag->printDataToBuf(&sb);
-		logf(LOG_TRACE, "\t\t%-12s: %s", getTagStrFromType(tag->m_type), sb.getBufStart());
-	}
+		logf(LOG_TRACE, "\ttagRec");
+		TagRec *tagRec = xmlDoc.getTagRec();
+		for (Tag *tag = tagRec->getFirstTag(); tag; tag = tagRec->getNextTag(tag)) {
+			SafeBuf sb;
+			tag->printDataToBuf(&sb);
+			logf(LOG_TRACE, "\t\t%-12s: %s", getTagStrFromType(tag->m_type), sb.getBufStart());
+		}
 
-	logf(LOG_TRACE, "\t");
+		logf(LOG_TRACE, "\t");
 
-	logf(LOG_TRACE, "Links info");
-	g_log.m_disabled = true;
-	Links *links = xmlDoc.getLinks();
-	g_log.m_disabled = false;
-	for (int i = 0; i < links->getNumLinks(); ++i) {
-		logf(LOG_TRACE, "\tlink      : %.*s", links->getLinkLen(i), links->getLinkPtr(i));
-
+		logf(LOG_TRACE, "Links info");
+		g_log.m_disabled = true;
+		Links *links = xmlDoc.getLinks();
+		g_log.m_disabled = false;
+		for (int i = 0; i < links->getNumLinks(); ++i) {
+			logf(LOG_TRACE, "\tlink      : %.*s", links->getLinkLen(i), links->getLinkPtr(i));
+		}
 	}
 	cleanup();
 
