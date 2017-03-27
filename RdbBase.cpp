5b47247ec55690f9b66535fec7fe959ed181ecd5
Ai Lin Chia <alc@privacore.com>
2017-03-27 14:06:52 +0200

    Fix delete. We can't delete void* ptr

--- RdbBase.cpp
+++ RdbBase.cpp
@@ -2675,7 +2675,7 @@ void RdbBase::generateGlobalIndex(void *item) {
 
 	log(LOG_INFO, "db: Processed job %p to generate global index", item);
 
-	delete item;
+	delete queueItem;
 }
 
 /// @todo ALC we should free up m_fileInfo[i].m_index->m_docIds when we don't need it, and load it back when we do
