mehmet-biter/open-source-search-engine

commit ed203f7f7f075c98374ff994688ab126dfa59f50

Removed Msg4::m_tmpBuf (Spider.cpp can handle memory allocation just fine without that hack)

Spider.cpp
@@ -1651,8 +1651,10 @@ public:
 };
 
 static void doneAddingSeedsWrapper(void *state) {
+	SafeBuf *sb = reinterpret_cast<SafeBuf*>(state);
 	// note it
 	log("basic: done adding seeds using msg4");
+	delete sb;
 }
 
 // . Collectiondb.cpp calls this when any parm flagged with
@@ -1756,7 +1758,7 @@ bool updateSiteListBuf ( collnum_t collnum ,
 	sc->m_siteListIsEmptyValid = true;
 
 	// use this so it will be free automatically when msg4 completes!
-	SafeBuf *spiderReqBuf = &sc->m_msg4x.m_tmpBuf;
+	SafeBuf *spiderReqBuf = new SafeBuf();
 
 	//char *siteList = cr->m_siteListBuf.getBufStart();
 
@@ -2014,7 +2016,12 @@ bool updateSiteListBuf ( collnum_t collnum ,
 
 	// use spidercoll to contain this msg4 but if in use it
 	// won't be able to be deleted until it comes back..
-	return sc->m_msg4x.addMetaList(spiderReqBuf, sc->m_collnum, sc, doneAddingSeedsWrapper, RDB_SPIDERDB);
+	if(!sc->m_msg4x.addMetaList(spiderReqBuf, sc->m_collnum, sc, doneAddingSeedsWrapper, RDB_SPIDERDB))
+		return false;
+	else {
+		delete spiderReqBuf;
+		return true;
+	}
 }
 
 // . Spider.cpp calls this to see if a url it wants to spider is

Msg4Out.cpp
@@ -394,9 +394,6 @@ bool Msg4::addMetaList2 ( ) {
 		return false;
 	}
 
-	// in case this was being used to hold the data, free it
-	m_tmpBuf.purge();
-
 	logTrace( g_conf.m_logTraceMsg4, "END - OK, true" );
 	return true;
 }

Msg4Out.h
@@ -31,8 +31,6 @@ public:
 	static bool isInLinkedList(const Msg4 *msg4);
 	static void storeLineWaiters();
 
-	SafeBuf m_tmpBuf;
-
 private:
 	bool addMetaList2();
 

commit abf8223b55932bad27aa682f1f3fc0d04a1d3ca6

Fix a bug introduced in ed203f7f7f075c98374ff994688ab126dfa59f50 where the wrong variable is passed as state causing wrong pointer to be deleted on callback

Spider.cpp
@@ -1145,7 +1145,7 @@ bool updateSiteListBuf ( collnum_t collnum ,
 
 	// use spidercoll to contain this msg4 but if in use it
 	// won't be able to be deleted until it comes back..
-	if(!sc->m_msg4x.addMetaList(spiderReqBuf, sc->m_collnum, sc, doneAddingSeedsWrapper, RDB_SPIDERDB))
+	if(!sc->m_msg4x.addMetaList(spiderReqBuf, sc->m_collnum, spiderReqBuf, doneAddingSeedsWrapper, RDB_SPIDERDB))
 		return false;
 	else {
 		delete spiderReqBuf;