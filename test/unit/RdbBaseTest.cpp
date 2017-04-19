5d94ea73f9ddf886c25206d5348cd6036a15757a
Ai Lin Chia <alc@privacore.com>
2017-04-19 16:20:24 +0200

    Use fileId instead of fileIdx as fileIdx could change due to merge.

--- test/unit/RdbBaseTest.cpp
+++ test/unit/RdbBaseTest.cpp
@@ -39,24 +39,25 @@ INSTANTIATE_TEST_CASE_P(RdbBasePosdbIndexSingleDoc,
 TEST_P(RdbBasePosdbIndexSingleDocTest, PosdbGenerateIndexSingleDocId) {
 	const collnum_t collNum = 0;
 	const int64_t docId = 1;
+	int32_t fileId = 0;
 
 	RdbBase *base = g_posdb.getRdb()->getBase(collNum);
 
-	ASSERT_EQ(0, base->addNewFile());
+	ASSERT_EQ(0, base->addNewFile(&fileId));
 	RdbIndex *index0 = base->getIndex(0);
 	int64_t termId0 = ::testing::get<0>(GetParam());
 	GbTest::addPosdbKey(index0, termId0, docId, 0, ::testing::get<0>(GetParam()) == POSDB_DELETEDOC_TERMID);
 	index0->writeIndex(true);
 	base->markNewFileReadable();
 
-	ASSERT_EQ(1, base->addNewFile());
+	ASSERT_EQ(1, base->addNewFile(&fileId));
 	RdbIndex *index1 = base->getIndex(1);
 	int64_t termId1 = ::testing::get<1>(GetParam());
 	GbTest::addPosdbKey(index1, termId1, docId, 0, ::testing::get<1>(GetParam()) == POSDB_DELETEDOC_TERMID);
 	index1->writeIndex(true);
 	base->markNewFileReadable();
 
-	ASSERT_EQ(2, base->addNewFile());
+	ASSERT_EQ(2, base->addNewFile(&fileId));
 	RdbIndex *index2 = base->getIndex(2);
 	int64_t termId2 = ::testing::get<2>(GetParam());
 	GbTest::addPosdbKey(index2, termId2, docId, 0, ::testing::get<2>(GetParam()) == POSDB_DELETEDOC_TERMID);
@@ -75,37 +76,37 @@ TEST_F(RdbBaseTest, PosdbUpdateIndex) {
 
 	RdbBase *base = g_posdb.getRdb()->getBase(collNum);
 
-	ASSERT_EQ(0, base->addNewFile());
+	ASSERT_EQ(0, base->addNewFile(&fileId));
 	RdbIndex *index0 = base->getIndex(0);
 	GbTest::addPosdbKey(index0, 'A', 1, 0);
 	index0->writeIndex(true);
 	base->markNewFileReadable();
 
-	ASSERT_EQ(1, base->addNewFile());
+	ASSERT_EQ(1, base->addNewFile(&fileId));
 	RdbIndex *index1 = base->getIndex(1);
 	GbTest::addPosdbKey(index1, 'B', 2, 0);
 	index1->writeIndex(true);
 	base->markNewFileReadable();
 
-	ASSERT_EQ(2, base->addNewFile());
+	ASSERT_EQ(2, base->addNewFile(&fileId));
 	RdbIndex *index2 = base->getIndex(2);
 	GbTest::addPosdbKey(index2, 'C', 3, 0);
 	index2->writeIndex(true);
 	base->markNewFileReadable();
 
-	ASSERT_EQ(3, base->addNewFile());
+	ASSERT_EQ(3, base->addNewFile(&fileId));
 	RdbIndex *index3 = base->getIndex(3);
 	GbTest::addPosdbKey(index3, 'D', 4, 0);
 	index3->writeIndex(true);
 	base->markNewFileReadable();
 
-	ASSERT_EQ(4, base->addNewFile());
+	ASSERT_EQ(4, base->addNewFile(&fileId));
 	RdbIndex *index4 = base->getIndex(4);
 	GbTest::addPosdbKey(index4, 'E', 5, 0);
 	index4->writeIndex(true);
 	base->markNewFileReadable();
 
-	ASSERT_EQ(5, base->addNewFile());
+	ASSERT_EQ(5, base->addNewFile(&fileId));
 	RdbIndex *index5 = base->getIndex(5);
 	GbTest::addPosdbKey(index5, 'F', 6, 0);
 	index5->writeIndex(true);
