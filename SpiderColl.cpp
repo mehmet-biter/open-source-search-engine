b378faef10daf9cb8940ce57e9b446cd29c6071e
Brian Rasmusson <br@privacore.com>
2017-07-04 12:05:08 +0200

    Initial work to store count of consecutive errors of the same kind:
    Use reserved byte in SpiderRequest for m_sameErrCount.
    Use reserved int32_t in SpiderRequest for m_prevErrCode.
    Replace unused pubDate in SpiderReply with m_sameErrCount and 3 reserved bytes.
    Add 'sameerrorcount' URL Filter expression to test on new sameErrCount value.
    Remove unused 'age' URL Filter expression that worked on unused pubDate.
    Add sameErrCount and prevErrCode to spiderdb dump output.

--- SpiderColl.cpp
+++ SpiderColl.cpp
@@ -2401,11 +2401,18 @@ bool SpiderColl::scanListForWinners ( ) {
 		}
 		// assign
 		sreq->m_siteNumInlinks = sni;
+
 		// store error count in request so xmldoc knows what it is
 		// and can increment it and re-add it to its spiderreply if
 		// it gets another error
 		if ( srep ) {
 			sreq->m_errCount = srep->m_errCount;
+
+			// Save error code of last reply in the request so we
+			// can compare with error code after next spider attempt.
+			sreq->m_prevErrCode = srep->m_errCode;
+			sreq->m_sameErrCount = srep->m_sameErrCount;
+
 
 			// . assign this too from latest reply - smart compress
 			// . this WAS SpiderReply::m_pubdate so it might be
@@ -2414,6 +2421,11 @@ bool SpiderColl::scanListForWinners ( ) {
 			sreq->m_contentHash32 = srep->m_contentHash32;
 			// if we tried it before
 			sreq->m_hadReply = true;
+		}
+		else {
+			sreq->m_errCount = 0;
+			sreq->m_sameErrCount = 0;
+			sreq->m_prevErrCode = 0;
 		}
 
 		// . get the url filter we match
