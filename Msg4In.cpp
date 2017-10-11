mehmet-biter/open-source-search-engine

commit 95023f6563906bd9beaa63c62103a14f91dda38d

Moved record-size-check and jump-to-next statements to more logical places (will also make sqlite changes easier to merge)

Msg4In.cpp
@@ -231,6 +231,10 @@ static bool Msg4In::addMetaList(const char *p, UdpSlot *slot) {
 
 		const char *rec = p;
 
+		// recSize can't go over pend
+		if(p + recSize > pend)
+			gbshutdownAbort(true);
+
 		// Sanity. Shut down if data sizes are wrong.
 		if( rdbId == RDB_TITLEDB ) {
 			Titledb::validateSerializedRecord( rec, recSize );
@@ -267,18 +271,10 @@ static bool Msg4In::addMetaList(const char *p, UdpSlot *slot) {
 			}
 		}
 
-		// recSize can't go over pend
-		if (p + recSize > pend) {
-			gbshutdownAbort(true);
-		}
-
 		// if we don't have data, recSize must be the same with keySize
 		if (rdb->getFixedDataSize() == 0 && recSize != rdb->getKeySize()) {
 			gbshutdownAbort(true);
 		}
-
-		// advance over the rec data to point to next entry
-		p += recSize;
 
 		// don't add to spiderdb when we're nospider host
 		if (!g_hostdb.getMyHost()->m_spiderEnabled && (rdbId == RDB_SPIDERDB || rdbId == RDB2_SPIDERDB2)) {
@@ -296,6 +292,9 @@ static bool Msg4In::addMetaList(const char *p, UdpSlot *slot) {
 		rdbItem.m_dataSizes += dataSize;
 
 		rdbItem.m_items.emplace_back(collnum, rec, recSize);
+
+		// advance over the rec data to point to next entry
+		p += recSize;
 	}
 
 	bool hasRoom = true;

commit 454cf12dc26f3d4696e3fd84c6fb3e62db50f256

Fix a bug introduced by commit 95023f6563906bd9beaa63c62103a14f91dda38d that causes Msg4 to be decoded incorrectly in query host when receiving spiderdb records

Msg4In.cpp
@@ -278,6 +278,8 @@ static bool Msg4In::addMetaList(const char *p, UdpSlot *slot) {
 
 		// don't add to spiderdb when we're nospider host
 		if (!g_hostdb.getMyHost()->m_spiderEnabled && (rdbId == RDB_SPIDERDB || rdbId == RDB2_SPIDERDB2)) {
+			// advance over the rec data to point to next entry
+			p += recSize;
 			continue;
 		}
 