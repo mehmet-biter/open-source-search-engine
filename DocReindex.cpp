91b7def61739876545d2b857b56c948aba3aa613
Ai Lin Chia <alc@privacore.com>
2017-12-05 12:49:52 +0100

    More fixes for docreindex

--- DocReindex.cpp
+++ DocReindex.cpp
@@ -20,6 +20,8 @@
 #include "XmlDoc.h"
 #include "Msg0.h"
 #include "RdbList.h"
+#include "Conf.h"
+#include "TitleRecVersion.h"
 
 DocReindex g_docReindex("docreindex.txt", false);
 DocReindex g_docReindexUrl("docreindexurl.txt", true);
@@ -49,6 +51,14 @@ DocProcessDocItem* DocReindex::createDocItem(DocProcess *docProcess, const std::
 
 void DocReindex::updateXmldoc(XmlDoc *xmlDoc) {
 	xmlDoc->m_indexCodeValid = false;
+
+#ifndef PRIVACORE_SAFE_VERSION
+	xmlDoc->m_version = g_conf.m_titleRecVersion;
+#else
+	xmlDoc->m_version = TITLEREC_CURRENT_VERSION;
+#endif
+
+	xmlDoc->m_versionValid = true;
 }
 
 void DocReindex::processDocItem(DocProcessDocItem *docItem) {
