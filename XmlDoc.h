aec712eea8665aeefa091ebfe0ba81ab216ba979
Ai Lin Chia <alc@privacore.com>
2018-01-19 16:17:40 +0100

    Revert "We should use tagrec based on current url to get site tag instead of tagrec based on first url"
    This reverts commit d4728fc24c369ce1b7dfb4b049aeeeeeb96b7448.

--- XmlDoc.h
+++ XmlDoc.h
@@ -395,7 +395,6 @@ public:
 	int64_t *getDocId ( ) ;
 	char *getIsIndexed ( ) ;
 	class TagRec *getTagRec ( ) ;
-	class TagRec *getCurrentTagRec ( ) ;
 	// non-dup/nondup addresses only
 	int32_t *getFirstIp ( ) ;
 	int32_t *getSiteNumInlinks ( ) ;
@@ -655,7 +654,6 @@ public:
 	HttpMime   m_mime;
 	TagRec     m_tagRec;
 	SafeBuf    m_tagRecBuf;
-	TagRec     m_currentTagRec;
 	SafeBuf    m_newTagBuf;
 	SafeBuf    m_fragBuf;
 	SafeBuf    m_wordSpamBuf;
@@ -681,7 +679,6 @@ public:
 	bool m_firstUrlHash64Valid;
 	bool m_docIdValid;
 	bool m_tagRecValid;
-	bool m_currentTagRecValid;
 	bool m_robotsTxtLenValid;
 	bool m_tagRecDataValid;
 	bool m_newTagBufValid;
