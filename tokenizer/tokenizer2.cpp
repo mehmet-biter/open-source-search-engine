7e3579397486193aad3cdcdc18809afa4c631d25
Ivan Skytte Jørgensen <isj-github@privacore.com>
2018-03-04 19:47:56 +0100

    tokenizer: Added another TODO

--- tokenizer/tokenizer2.cpp
+++ tokenizer/tokenizer2.cpp
@@ -36,6 +36,7 @@ void plain_tokenizer_phase_2(lang_t lang, const char *country_code, TokenizerRes
 	combine_hyphenated_words(tr);
 	recognize_telephone_numbers(tr,lang,country_code);
 	//TODO: recognize_numbers(tr,lang,country_code)
+	//TODO: support use by query with quotation marks for suppressing alternatives (eg, "john's cat" should be not generate the "johns" special bigram
 }
 
 
