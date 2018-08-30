a28cea72509d26745dd3063bca0ad3be47a2f3b9
Ivan Skytte Jørgensen <isj-github@privacore.com>
2018-08-30 13:41:38 +0200

    More unittest for domain/TLD/url

--- test/unit/getProbableDocIdTest.cpp
+++ test/unit/getProbableDocIdTest.cpp
@@ -20,3 +20,13 @@ TEST(ProbableDocIdTest, TwoLevelTLD) {
 	EXPECT_EQ(Docid::getProbableDocId("www.example.co.dk"),271301917664);
 	EXPECT_EQ(Docid::getProbableDocId("www.example.dtu.ua"),202434669729);
 }
+TEST(ProbableDocIdTest, WholeURLs) {
+	EXPECT_EQ(Docid::getProbableDocId("http://www.example.co.uk/snarf/foo.html"),252979708372);
+	EXPECT_EQ(Docid::getProbableDocId("https://www.example.co.uk/snarf/foo.html"),150254485977);
+	EXPECT_EQ(Docid::getProbableDocId("http://www.example.co.uk/foo.html"),161048396274);
+	EXPECT_EQ(Docid::getProbableDocId("http://www.example.co.uk/snarf/foo.html?boo=bar"),59068187089);
+	EXPECT_EQ(Docid::getProbableDocId("http://www.example.co.uk:8080/snarf/foo.html"),40110146042);
+	EXPECT_EQ(Docid::getProbableDocId("http://www.example.co.uk"),131891560943);
+	EXPECT_EQ(Docid::getProbableDocId("http://192.0.2.17/snarf/foo.html"),2217203697);
+	EXPECT_EQ(Docid::getProbableDocId("http://192.0.2.17:8090/snarf/foo.html"),94908962801);
+}
